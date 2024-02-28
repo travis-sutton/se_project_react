119c135b2f18e4cac77f5c356193eda08ba132f3
TravisSutton <travis.sutton8813@gmail.com>
2024-02-27 18:00:31 -0600

    fix file naming issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
-import App from "./Components/App/App";
+import App from "./components/App/App";
 
 const root = document.getElementById("root");
 createRoot(root).render(<App />);
