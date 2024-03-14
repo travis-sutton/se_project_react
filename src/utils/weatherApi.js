2419781c27aaccae58b1b8fb126e1eaa8d8f5ccd
TravisSutton <travis.sutton8813@gmail.com>
2024-03-14 14:58:05 -0500

    remove error from api because it is handled in App, combined the weather and location into one fetchData function

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -3,17 +3,17 @@ import { handleServerResponse } from "./api";
 export const coordinates = { longitude: -86.76, latitude: 36.17 };
 export const APIkey = "18f949f1a31af325db382aae28a78f6b";
 
-export const getForecastWeather = () => {
-  const weatherApi = fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}&units=imperial&appid=${APIkey}`
-  )
-    .then(handleServerResponse)
-    .catch((error) => {
-      console.error("Error fetching weather data:", error);
-    });
+const fetchData = () => {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}&units=imperial&appid=${APIkey}`;
+  const locationUrl = `https://api.openweathermap.org/geo/1.0/reverse?lat=${coordinates.latitude}&lon=${coordinates.longitude}&limit=1&appid=${APIkey}`;
 
-  return weatherApi;
+  return {
+    getForecastWeather: () => fetch(weatherUrl).then(handleServerResponse),
+    getLocationData: () => fetch(locationUrl).then(handleServerResponse),
+  };
 };
+
+export const { getForecastWeather, getLocationData } = fetchData();
 
 export const parseWeatherData = (data) => {
   const main = data.main;
@@ -26,20 +26,6 @@ export const parseWeatherData = (data) => {
   };
 
   return weather;
-};
-
-export const getLocationData = (coordinates, APIkey) => {
-  const locationApi = fetch(
-    `https://api.openweathermap.org/geo/1.0/reverse?lat=${coordinates.latitude}&lon=${coordinates.longitude}&limit=1&appid=${APIkey}`
-  )
-    .then(handleServerResponse)
-
-    .catch((error) => {
-      console.error("Error fetching location data:", error);
-      return "Not Found";
-    });
-
-  return locationApi;
 };
 
 export const parseLocationData = (data) => {
